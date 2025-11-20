6b818547dc040b3440d9e75114905d2e3a853997
Kolossus-AI <system@kolossus.ai>
2025-11-20 11:37:56 +0530

    Create k_test.py
    this is test file

---
+++ k_test.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater or equal to a")
+
